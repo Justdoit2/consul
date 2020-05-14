8cb4f0f67ceb303b0592d069e8aef14fd99e839b
Chris Piraino <cpiraino@hashicorp.com>
2020-05-14 14:49:57 -0500

    Do not return an error if requested service is not a gateway
    This commit converts the previous error into just a Warn-level log
    message. By returning an error when the requested service was not a
    gateway, we did not appropriately update envoy because the cache Fetch
    returned an error and thus did not propagate the update through proxycfg
    and xds packages.

--- agent/consul/internal_endpoint.go
+++ agent/consul/internal_endpoint.go
@@ -414,8 +414,15 @@ func (m *Internal) GatewayServices(args *structs.ServiceSpecificRequest, reply *
 				}
 			}
 
+			// Welog a warning here to indicate that there is a potential
+			// misconfiguration. We explicitly do NOT return an error because this
+			// can occur in the course of normal operation by deleting a
+			// configuration entry or starting the proxy before registering the
+			// config entry.
 			if !found {
-				return fmt.Errorf("service %q is not a configured terminating-gateway or ingress-gateway", args.ServiceName)
+				m.logger.Warn("service is not a configured terminating-gateway or ingress-gateway",
+					"service", args.ServiceName,
+				)
 			}
 
 			index, services, err = state.GatewayServices(ws, args.ServiceName, &args.EnterpriseMeta)
